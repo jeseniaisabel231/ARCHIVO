2fbcf0aab5ebfc797a16526157946c74768b5b8e
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-01-23 00:52:54 -0500

    version 3

---
+++ src/form3.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class form3 {
+    public JPanel lol;
+    private JButton subirArchivoButton;
+    private JButton agregarArchivoButton;
+    private JButton cerrarButton;
+    private JButton inicioButton;
+    private JButton tareaButton;
+    private JButton completadaButton;
+     JPanel panel3;
+}
