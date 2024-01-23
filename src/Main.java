39d47c1a8b808268d234a78892d5fcfb4972fbd2
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-01-22 23:57:18 -0500

    form3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("primera pantalla");
-        frame.setContentPane(new form2().panel2);
+        frame.setContentPane(new form3().panel3);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
