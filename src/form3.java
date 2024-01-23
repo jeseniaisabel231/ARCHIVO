0bd2b6d85fc1ddc7d3dcba517fef5777d37a06be
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2024-01-23 17:13:03 -0500

    tabla 2

---
+++ src/form3.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class form3 {
+    private JButton archivoButton;
+    private JTextField textField1;
+    private JTable table1;
+    private JCheckBox CHECKCheckBox;
+    private JCheckBox CROSSCheckBox;
+}
