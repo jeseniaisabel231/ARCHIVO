4245dd0af15fd603003efaa718539a8b994b0bac
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2024-01-23 07:28:52 -0500

    version5

--- src/form2.java
+++ src/form2.java
@@ -6,4 +6,13 @@ public class form2 {
     private JButton agregarArchivoButton;
     private JButton cerrarButton;
     private JButton inicioButton;
+
+    public void mostrarPantalla2() {
+        JFrame aplicacion = new JFrame("Pantalla 2");
+        aplicacion.setContentPane(new form2().panel2);
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aplicacion.setSize(600,480);
+        aplicacion.pack();
+        aplicacion.setVisible(true);
+    }
 }
